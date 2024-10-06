5975a0a5fe691a44602556ad4e04660a52ae5f19
gobbe <megobbt@gmail.com>
2024-10-06 10:47:41 +0200

    test

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use bevy::{prelude::*, transform::commands, window::PrimaryWindow};
+
+fn main() {
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, spawn_camera)
+    .add_systems(Startup, spawn_player)
+    .run();
+}
+
+#[derive(Component)]
+pub struct Player {}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+)   {
+        let window = window_query.get_single().unwrap();
+
+        commands.spawn(
+            (
+                SpriteBundle{
+                    transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+                    texture: asset_server.load("sprites/skeleton.png"),
+                    ..default()
+                },
+                Player{},
+            )
+        );
+    }
+
+pub fn spawn_camera(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+)   {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(
+        Camera2dBundle{
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            ..default()
+        }
+    );
+}
